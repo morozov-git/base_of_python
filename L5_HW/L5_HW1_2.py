666b32f33c6473e46e1d1546cb200f57dd74d2d8
morozov.da <morozov.da@gmail.com>
2021-03-04 23:57:35 +0300

    update L5_HW1_2 add console printscreen

--- L5_HW/L5_HW1_2.py
+++ L5_HW/L5_HW1_2.py
@@ -5,19 +5,25 @@ def nums_gen(n):
 		if nums % 2 == 1:
 			yield nums
 
+if __name__ == '__main__':
+	nums = nums_gen(int(input("Введите количество чисел: ")))
+	#nums = nums_gen(23)
+	print(next(nums), next(nums), next(nums), next(nums))
+	print(type(nums), *nums)
 
-nums = nums_gen(int(input("Введите количество чисел: ")))
-#nums = nums_gen(23)
-print(next(nums), next(nums), next(nums), next(nums))
-
-print(type(nums), *nums)
 
 
-#n = 23
-n = int(input("Введите количество чисел: "))
+# Вариант без yield
 
-nums = (num for num in range(n+1) if num % 2 == 1)
-print(next(nums), next(nums), next(nums), next(nums))
-print(type(nums), *nums)
+def nums_gen2(n):
+	nums = (num for num in range(n+1) if num % 2 == 1)
+	return nums
 
+# print(next(nums), next(nums), next(nums), next(nums))
+# print(type(nums), *nums)
+if __name__ == '__main__':
+	n = 23
+	# n = int(input("Введите количество чисел: "))
+	#print(next(nums), next(nums), next(nums), next(nums))
+	print(type(nums_gen(n)), *nums_gen(n))
 
