82c9b9198619ec71f3c63a2d26db329e073bfbd8
morozov.da <morozov.da@gmail.com>
2021-03-09 20:54:46 +0300

    add L6_HW6_show_sales.py

---
+++ L6_HW/L6_HW6_show_sales.py
@@ -0,0 +1,33 @@
+from sys import argv
+#import decimal
+
+#argv = [1, 3, 10]
+#argv = [1, 3]
+#argv = [1]
+#argv = []
+with open("bakery.csv", "r", encoding="utf-8") as bakery:
+	if len(argv) == 1:
+		print(bakery.read(), end="")
+	if len(argv) == 2:
+		i = 1
+		while i != int(argv[1]):
+			bakery.readline()
+			i += 1
+		print(bakery.read(), end="")
+	if len(argv) == 3:
+		i = 1
+		l = int(argv[2]) - int(argv[1]) + 1
+		while i != int(argv[1]):
+			bakery.readline()
+			i += 1
+		while l != 0:
+			print(bakery.readline(), end="")
+			l -= 1
+
+
+if __name__ == '__main__':
+	#print('Сумма продаж: ', argv[1], ' записана в bakery.csv', sep="")
+	#print(bakery.readline())
+	print('END')
+	print(argv)
+
