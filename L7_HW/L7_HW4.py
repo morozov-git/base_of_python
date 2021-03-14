713d47dd155c8a65ee69c111c3b51165d2db7b41
morozov.da <morozov.da@gmail.com>
2021-03-15 00:47:29 +0300

    update L7_HW4

--- L7_HW/L7_HW4.py
+++ L7_HW/L7_HW4.py
@@ -3,21 +3,22 @@ import os.path
 
 import django
 
-files_size_dict = {100:0, 1000:0, 10000:0, 100000:0}
+files_size_dict = {10:0, 100:0, 1000:0, 10000:0}
+#files_size_dict = {}
 root_dir = django.__path__[0]
 for root, dirs, files in os.walk(root_dir):
 	for file in files:
-		file_size = os.path.getsize(root + '/' + file)
-		if 0 < file_size < 101:
-			files_size_dict[100] = files_size_dict[100] + 1
-		if 101 < file_size < 1001:
-			files_size_dict[1000] = files_size_dict[1000] + 1
+		file_size = os.path.getsize(root + '/' + file) / 2 ** 10
+		if 0 < file_size < 11:
+			files_size_dict[10] += 1
+		if 11 < file_size < 101:
+			files_size_dict[100] += 1
+		if 100 < file_size < 1001:
+			files_size_dict[1000] += 1
 		if 1000 < file_size < 10001:
-			files_size_dict[10000] = files_size_dict[10000] + 1
-		if 10000 < file_size < 100001:
-			files_size_dict[100000] = files_size_dict[100000] + 1
+			files_size_dict[10000] += 1
 
 
-#		print(file_size)
-
+		print(file_size)
+print(f'\nСтатистика по размерам файлов в КБ для папки: \n{root_dir}', sep='')
 print(files_size_dict)
