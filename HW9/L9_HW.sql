a0e66ceda2a9556d6d5ba0136a1674438f841000
morozov.da <morozov.da@gmail.com>
2021-06-02 19:05:38 +0300

    Add Fibonacci

--- HW9/L9_HW.sql
+++ HW9/L9_HW.sql
@@ -230,10 +230,30 @@ VALUES
  
  
 
-
-
-
 -- 	3. (по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. 
 -- Числами Фибоначчи называется последовательность в которой число равно сумме двух предыдущих чисел. 
 -- Вызов функции FIBONACCI(10) должен возвращать число 55.
 
+ 
+DROP FUNCTION IF EXISTS fibonacci;
+DELIMITER //
+CREATE FUNCTION fibonacci (value INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+	SET @n = value;
+	SET @a = 0;
+	SET @b = 0;
+	SET @f = 0;
+	WHILE @n > 0 DO
+		SET @f = @a + @b + 1;
+		SET @a = @b - 1;
+		SET @b = @f;
+		SET @n = @n - 1;
+	END WHILE;
+RETURN @f;
+END//
+DELIMITER ;
+
+
+ 
+ 
