0ef31428df68caaf94ee2150b2582279b6c6e0c9
morozov.da <morozov.da@gmail.com>
2021-06-21 05:31:12 +0300

    Update L1_HW9

--- Python_Alg_L1_HW/python_alg_L1_HW9.py
+++ Python_Alg_L1_HW/python_alg_L1_HW9.py
@@ -25,3 +25,21 @@ while True:
 		else:
 			print(f"Среднее число: {c}")
 			break
+
+
+# другой вариант немного облегченный
+
+# while True:
+# 	numbers = input("Введите через запятую три разных целых числа: ")
+# 	a = numbers.split(',')
+# 	if len(a) == 3 and a[0].isdecimal() == a[1].isdecimal() == a[2].isdecimal() == True:
+# 		a, b, c = int(a[0]), int(a[1]), int(a[2])
+# 		if (a < b and a > c) or (a < c and a > b):
+# 			print(f"Среднее число: {a}")
+# 			break
+# 		elif (b < a and b > c) or (b < c and b > a):
+# 			print(f"Среднее число: {b}")
+# 			break
+# 		else:
+# 			print(f"Среднее число: {c}")
+# 			break
